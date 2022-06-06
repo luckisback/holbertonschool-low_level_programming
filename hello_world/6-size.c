ec5308e66a8533fdc781be57d442a02d47e7bd49
root <root@PPR1.localdomain>
2022-06-06 19:44:48 +0200

    programm ends now with "return 0"

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -19,4 +19,6 @@ printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(i));
 printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(l));
 printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(d));
 printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(f));
+
+return (0);
 }
