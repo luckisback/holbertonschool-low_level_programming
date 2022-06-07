16b871749373d06c82ab1c91639c31a8a6163db7
root <root@PPR1.localdomain>
2022-06-07 21:04:55 +0200

    udate

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -26,7 +26,7 @@ int main(void)
 			printf("Last digit of n is %d and is 0\n", n);
 		else
 		{
-			if (n > 6 && n != 0)
+			if (n < 6 && n > 0)
 				printf("Last digit of n is %d and is less than 6 and not 0\n", n);
 		}
 	}
