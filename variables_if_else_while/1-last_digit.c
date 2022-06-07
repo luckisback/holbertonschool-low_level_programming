8886b0802719ef9b92e7f61c8848493461e5ed13
root <root@PPR1.localdomain>
2022-06-07 23:14:42 +0200

    3 if statments is the best

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -4,9 +4,9 @@
 
 /* more headers goes there */
 /**
-* main - Entry point
-* Return: Always 0 (Success)
-*/
+ * main - Entry point
+ * Return: Always 0 (Success)
+ */
 /* betty style doc for function main goes there */
 
 int main(void)
@@ -17,18 +17,11 @@ int main(void)
 	n = rand() - RAND_MAX / 2;
 
 	/* your code goes there */
-
-	if (n > 5)
-		printf("Last digit of n is %d and is greater than 5\n", n);
-	else
-	{
-		if (n == 0)
-			printf("Last digit of n is %d and is 0\n", n);
-		else
-		{
-			if (n < 6 && n > 0)
-				printf("Last digit of n is %d and is less than 6 and not 0\n", n);
-		}
-	}
-	return (0);
+if (n > 5)
+	printf("Last digit of n is %d and is greater than 5\n", n);
+if (n == 0)
+	printf("Last digit of n is %d and is 0\n", n);
+if (n < 6 && n > 0)
+	printf("Last digit of n is %d and is less than 6 and not 0\n", n);
+return (0);
 }
