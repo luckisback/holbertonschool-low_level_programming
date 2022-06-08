d1130a9110cb48ffa122035e9c789a50beb0cbb4
root <root@PPR1.localdomain>
2022-06-08 22:51:11 +0200

    dual digit

--- variables_if_else_while/100-print_comb3.c
+++ variables_if_else_while/100-print_comb3.c
@@ -17,15 +17,25 @@ int main(void)
 	{
 		while (r < 58)
 		{
-			putchar(l);
-			putchar(r);
-			putchar(44);
-			putchar(32);
+			if (l == 56 && r == 57)
+			{
+				putchar(l);
+				putchar(r);
+			}
+			if (l <= 55 && r <= 56)
+			{
+				putchar(l);
+				putchar(r);
+				putchar(44);
+				putchar(32);
+			}
+
 			r = r + 1;
 		}
 
 		l = l + 1;
 		r = l + 1;
 	}
+	printf("\n");
 	return (0);
 }
