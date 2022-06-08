bcc57c337c7f0fb7b0caeafd261f58b1125e0061
root <root@PPR1.localdomain>
2022-06-08 19:16:45 +0200

    my digit combinaision

---
+++ variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+/**
+  * main - Entry point
+  * Return: Alwayse O (Success)
+  */
+
+int main(void)
+{
+	int l;
+	int r;
+
+	l = 48;
+	r = 49;
+
+	while (l < 57)
+	{
+		while (r < 58)
+		{
+			putchar(l);
+			putchar(r);
+			putchar(44);
+			putchar(32);
+			r = r + 1;
+		}
+
+		l = l + 1;
+		r = l + 1;
+	}
+	return (0);
+}
