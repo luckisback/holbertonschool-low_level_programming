721f3b3cb65cdc767b53ab50f9e7017c3cf50011
root <root@PPR1.localdomain>
2022-06-10 17:58:01 +0200

    ABS

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+* _abs - Function that computes the absolute value of an integer.
+* @i: Farater that print the value of variable
+* Return: retuns the absolut value of "i"
+*/
+
+int _abs(int i)
+{
+	if (i < 0)
+		i *= -1;
+	return (i);
+}
+
