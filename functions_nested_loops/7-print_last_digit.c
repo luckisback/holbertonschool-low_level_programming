86d85434112788a2ca8ef3ba4511f4cc1d0f24d5
root <root@PPR1.localdomain>
2022-06-12 09:01:20 +0200

    function for last digit

--- functions_nested_loops/7-print_last_digit.c
+++ functions_nested_loops/7-print_last_digit.c
@@ -5,14 +5,13 @@
  * @n: Variable representing the last digit
  * Return: Permit to return the value of n
  */
+
 int print_last_digit(int n)
 {
 	int i;
+	int d;
 
 	i = n % 10;
-
-	if (i <= 0 && i <= 9)
-	{
-		return (i);
-	}
+	d = _putchar('0' + i);
+	return (c);
 }
