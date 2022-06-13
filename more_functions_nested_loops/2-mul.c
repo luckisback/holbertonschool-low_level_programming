11602cc594db9e167e5b4b6e5e83886f2f7b2b91
root <root@PPR1.localdomain>
2022-06-13 14:38:20 +0200

    multiplation of 2 integers

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * mul - multiplies 2 integers
+ * @a: first argument
+ * @b: second argument
+ * Return: return the result
+ */
+
+int mul(int a, int b)
+{
+	int m;
+
+	m = (a * b);
+	return (m);
+}
