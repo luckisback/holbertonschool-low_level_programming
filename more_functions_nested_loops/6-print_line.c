8e7fbf931a2ccdf39fe7bc24fe15d2e68e07afc3
root <root@PPR1.localdomain>
2022-06-13 19:19:03 +0200

    print line

--- more_functions_nested_loops/6-print_line.c
+++ more_functions_nested_loops/6-print_line.c
@@ -15,13 +15,6 @@ void print_line(int n)
 
 	if (n > 0)
 	{
-		if (i == 2)
-			for (i = 1; i <= 2; i++)
-				_putchar(95);
-		if (i == 10)
-			for (i = 1; i <= 10; i++)
-				_putchar(95);
-
 		for (i = 1; i <= n; i++)
 		_putchar(95);
 	}
