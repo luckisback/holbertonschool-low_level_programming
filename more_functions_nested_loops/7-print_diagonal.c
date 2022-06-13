50416ff24bc6395fbf8b67efa15a9f42fc95e496
root <root@PPR1.localdomain>
2022-06-13 22:58:22 +0200

    print diagonal

--- more_functions_nested_loops/7-print_diagonal.c
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -12,18 +12,17 @@ void print_diagonal(int n)
 	int i;
 	int x;
 
-	i = 0;
-	x = 0;
-
-	if (n <= 0)
+	if (n > 0)
 	{
-		_putchar('\n');
+		for (i = 0; i < n; i++)
+		{
+			for (x = 0; x < i; x++)
+				_putchar(32);
+
+			_putchar(92);
+			_putchar('\n');
+		}
 	}
 	else
-	{
-		while (x < n)
-		{
-			_putchar(92);
-			_putchar(32);
-
-
+		_putchar('\n');
+}
