de55a9ddf2e8dda0e77058c7954fb240dee38977
root <root@PPR1.localdomain>
2022-06-15 00:37:19 +0200

    triangle

--- more_functions_nested_loops/10-print_triangle.c
+++ more_functions_nested_loops/10-print_triangle.c
@@ -3,7 +3,7 @@
 /**
  * print_triangle - function that print a triangle
  * _putchar - function that trheat the characters
- *
+ * @size:  argument used in the programm
  */
 
 void print_triangle(int size)
@@ -12,16 +12,23 @@ void print_triangle(int size)
 	int i = 0;
 	int z = 0;
 
-	for (x = 1; x <= size; x++)
+	if (size <= 0)
 	{
-		for (i = 1; i <= (size - x); i++)
-		{
-			_putchar(32);
-		}
-		for (z = 1; z <= x; z++)
-		{
-			_putchar(35);
-		}
 		_putchar('\n');
+	}
+	else
+	{
+		for (x = 1; x <= size; x++)
+		{
+			for (i = 1; i <= (size - x); i++)
+			{
+				_putchar(32);
+			}
+			for (z = 1; z <= x; z++)
+			{
+				_putchar(35);
+			}
+			_putchar('\n');
+		}
 	}
 }
