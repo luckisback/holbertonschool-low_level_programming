169dce84cc9bac4e95ff1336879d1350f85e7958
root <root@PPR1.localdomain>
2022-06-14 15:24:26 +0200

    printing Fizz Buzz

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -10,9 +10,9 @@ int main(void)
 {
 	int i;
 
-	i = 0;
+	i = 1;
 
-	while (i != 101)
+	while (i <= 100)
 	{
 		if (i % 3 == 0 && i % 5 == 0)
 			printf("FizzBuzz ");
@@ -22,7 +22,9 @@ int main(void)
 			printf("Fizz ");
 		else
 			printf("%d ", i);
+	
 		i++;
 	}
+	printf("\n");
 	return (0);
 }
