e780d8c47464cc0b131c3576dd3fd45fac5cc8f8
root <root@PPR1.localdomain>
2022-06-14 07:47:09 +0200

    print squre

---
+++ more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_square - function that prints a square
+ * @size: argument
+ * _putchar - function that prints characters
+ *
+ */
+
+void print_square(int size)
+{
+	int x;
+	int y;
+
+	for (y = 0; y < size; y++)
+	{
+		for (x = 0; x < size; x++)
+		{
+			_putchar(35);
+		}
+		_putchar('\n');
+	}
+}		
