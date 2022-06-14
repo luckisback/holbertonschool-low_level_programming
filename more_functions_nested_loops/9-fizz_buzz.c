8a5dd111d75e24ff8eafd938e10f62f4ce00f79a
root <root@PPR1.localdomain>
2022-06-14 12:27:40 +0200

    function Fizz Buzz

---
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * main - Fuction that permit to overcome the challenge
+ *
+ * Return: 0 for (Success
+ */
+
+int main(void)
+{
+	int i;
+
+	i = 0;
+
+	while (i != 101)
+	{
+		if (i % 3 == 0 && i % 5 == 0)
+			printf("FizzBuzz ");
+		else if (i % 5 == 0)
+			printf("Buzz ");
+		else if (i % 3 == 0)
+			printf("Fizz ");
+		else
+			printf("%d ", i);
+		i++;
+	}
+	return (0);
+}
