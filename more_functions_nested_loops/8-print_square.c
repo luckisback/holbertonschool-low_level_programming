e3185985165fb1289ec87b714c0e7c3140e8e294
root <root@PPR1.localdomain>
2022-06-14 08:57:13 +0200

    print square

--- more_functions_nested_loops/8-print_square.c
+++ more_functions_nested_loops/8-print_square.c
@@ -12,13 +12,12 @@ void print_square(int size)
 	int x;
 	int y;
 
-	for (y = 0; y < size; y++)
+	for (y = 1; y <= size; y++)
 	{
-		for (x = 0; x < size; x++)
+		for (x = 1; x <= size; x++)
 		{
 			_putchar(35);
 		}
 		_putchar('\n');
 	}
-	_putchar('\n');
 }
