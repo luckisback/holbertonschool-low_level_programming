a2c43ba99ba9b936e3883635b2ac0bc1c597bb55
root <root@PPR1.localdomain>
2022-06-15 18:48:00 +0200

    addition

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * add - function that adds two integers and returns the result.
+ * @x: the first argument
+ * @y: seconde argument
+ * Return: provids the result of maths
+ */
+
+int add(int x, int y)
+{
+	int result;
+
+	result = (x + y);
+	return (result);
+}
