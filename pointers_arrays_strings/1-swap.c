791fbc18079449e1115faf2562e9936cc9494a3f
root <root@PPR1.localdomain>
2022-06-15 09:46:07 +0200

    swap the values of 2 var

---
+++ pointers_arrays_strings/1-swap.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * swap_int - function that swaps the values of two integers.
+ * @a: first argument
+ * @b: second argument
+ *
+ */
+
+void swap_int(int *a, int *b)
+{
+	int tmp;
+
+	tmp = *a;
+	*a = *b;
+	*b = tmp;
+}
+
