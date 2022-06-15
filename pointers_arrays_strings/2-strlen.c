de6e0cf3f814708c39da96114543f52103d812a4
root <root@PPR1.localdomain>
2022-06-15 10:51:10 +0200

    Count the number of address

---
+++ pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _strlen -  function that returns the length of a string
+ *
+ * @s: argument (pointer)
+ * Return: provid the result of the function
+ */
+
+int _strlen(char *s)
+{
+	int Nbr_add = 0;
+
+	while (Nbr_add[s] != '\0')
+		Nbr_add++;
+	return (Nbr_add);
+}
