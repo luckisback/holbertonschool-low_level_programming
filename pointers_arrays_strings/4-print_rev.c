3c0fd9563940b21f7b465ce793f158c3aaeaf0b4
root <root@PPR1.localdomain>
2022-06-15 13:59:07 +0200

    revers puts

---
+++ pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_rev - function that prints a string, in reverse
+ * @s: argument
+ *
+ */
+
+void print_rev(char *s)
+{
+	int i = 0;
+	int r;
+
+	while (i[s] != '\0')
+	{
+		i++;
+	}
+	for (r = i; r >= 0; r--)
+		_putchar(r[s]);
+	_putchar('\n');
+}
