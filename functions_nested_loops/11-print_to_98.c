6edbcb50d3c77726aa9f12804b2719cd68332c72
root <root@PPR1.localdomain>
2022-06-15 20:54:58 +0200

    printing num between n and 98

---
+++ functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+* print_to_98 - function that prints all natural numbers from n to 98
+* @n: argument
+* Return: provids the result
+*
+*/
+
+void print_to_98(int n)
+{
+	while (n < 98)
+	{
+		printf("%d, ", n);
+		n++;
+	}
+	while (n > 98)
+	{
+		printf("%d, ", n);
+		n -= 1;
+	}
+	if (n == 98)
+		printf("98");
+	printf("\n");
+}
